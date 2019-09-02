f1feada7c778947cb8723593a4b4b24b522e42ef
Jeremy Parris <jeremy.parris@a-f-p.com>
2019-09-02 14:35:49 -0500

    wwwroot folder restructures and added meta tags

--- src/app/Contact/contact.component.ts
+++ src/app/Contact/contact.component.ts
@@ -17,10 +17,14 @@ export class ContactComponent {
     mapType = 'roadmap';
     contactForm : FormGroup;
     submitted : boolean;
+    isEmailSent : boolean;
+    showLoader : boolean;
 
     constructor(private contactService : ContactService, private formBuilder : FormBuilder){
         this.message = {FullName: "", PhoneNumber: "", Email : "", Description: ""};
         this.submitted = false;
+        this.isEmailSent = false;
+        this.showLoader = false;
 
         this.contactForm = this.formBuilder.group({
             fullName: ['', Validators.required],
@@ -33,7 +37,7 @@ export class ContactComponent {
     get f() { return this.contactForm.controls; }
     public sendMessage(){
         this.submitted = true;
-
+        this.showLoader = true;
         if(this.contactForm.invalid){
             return;
         }
@@ -41,8 +45,12 @@ export class ContactComponent {
         this.contactService.sendMessage(this.message).subscribe(() => {
             this.message = {FullName: "", PhoneNumber: "", Email : "", Description: ""};
             this.submitted = false;
+            this.isEmailSent = true;
+            this.showLoader = false;
         }, error => {
             this.errorMessage = error;
+            this.showLoader = false;
+            this.submitted = false;
         });
     }
 }
